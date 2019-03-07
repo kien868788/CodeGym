c1c4da1fb068aa54e50ef56f0fa560fa151cc618
Tran Trung Kien <kien868788@gmail.com>
2019-03-07 10:57:52 +0700

    Push choi

--- CaseStudio/Components/Bar.js
+++ CaseStudio/Components/Bar.js
@@ -1,5 +1,5 @@
-function Bar(x,y,color,width,height) {
-  Shape.call(this,x,y,0,0,color);
+function Bar(x, y, color, width, height) {
+  Shape.call(this, x, y, 0, 0, color);
   this.width = width;
   this.height = height;
 }
@@ -9,10 +9,12 @@ Bar.prototype = Object.create(Shape.prototype);
 Bar.prototype.draw = function() {
   ctx.beginPath();
   ctx.fillStyle = this.color;
-  ctx.fillRect(this.x,this.y,this.width,this.height);
+  ctx.fillRect(this.x, this.y, this.width, this.height);
 }
 
-Bar.prototype.move = function({keyCode}) {
+Bar.prototype.move = function({
+  keyCode
+}) {
   var step = 50;
   if (keyCode == '37') {
     if (this.x - step <= 0) {
